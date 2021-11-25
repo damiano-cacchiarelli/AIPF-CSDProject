3c2126dd73344ac33908382a81bb65c611c856e3
Damiano Cacchiarelli <damiano.cacchiarelli@studenti.unicam.it>
2021-11-25 18:41:21 +0100

    New class ProgressPercentageIndicator

---
+++ AIPF/MLManager/Modifiers/ProgressPercentageIndicator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AIPF.MLManager.Modifiers
+{
+    public class ProgressPercentageIndicator<I> : ProgressIndicator<I> where I : class, ICopy<I>, new()
+    {
+        public int TotalCount { get; set; }
+
+        public ProgressPercentageIndicator(string processName, bool toString = false) : base(processName, toString)
+        {
+        }
+
+        protected override void Log()
+        {
+            if (Processed == 0) Console.WriteLine("");
+            Console.WriteLine($"Progress Indicator work! {processName} - { (Processed++ / TotalCount * 100) } %");
+        }
+    }
+}
